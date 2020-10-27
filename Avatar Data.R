d95d7d314d6f394fcc0ec77f52091d9df54e1e05
elianemitchell <62623125+elianemitchell@users.noreply.github.com>
2020-10-27 15:49:40 -0400

    Update Avatar Data.R

--- Avatar Data.R
+++ Avatar Data.R
@@ -2,69 +2,45 @@
 install.packages("tidytuesdayR")
 tuesdata <- tidytuesdayR::tt_load('2020-08-11')
 avatar <- tuesdata$avatar
+devtools::install_github("averyrobbins1/appa")
 
 # Packages
 library(tidyverse)
-devtools::install_github("averyrobbins1/appa")
-install.packages("gt")
 library(gt)
-install.packages("showtext")
 library(showtext)
-library(appa)
-avatar <- appa::appa
-glimpse(avatar)
-install.packages("data.table")
-library(data.table)
-install.packages("skimr")
-library(skimr)
-library(ggplot2)
-font_add_google(name = "Linotype Syntax Lapidar Serif Display", 
-                family = "linotype")
-install.packages("extrafont")
-library(extrafont)
-y
-#Install Emojis package
+
+# Install Emojis package
 devtools::install_github("hadley/emo")
 
-
-avatar %>% head() %>% gt()
-
-#Proposed Steps
-# 1. keep columns for book and book num, chapter num, director,
-# and IMDB rating
-# 2. May need to group books together? so that 
-# there aren't so many repeats?
-# 3. check that gt table makes a simple table correctly with
-# the book title, its number, the director and the avg IMDB
-# rating 
-# 4. add in bar charts of IMDB ratings by chapter num
-
+# Drop Unwanted Columns 
 drop <- c("chapter", "character", "full_text",
           "character_words", "scene_description",
           "writer", "id")
 df = avatar[,!(names(avatar) %in% drop)]
+
+# Drop Duplicated Rows
 duplicated(df)
 clean_avatar <- df[!duplicated(df), ]
-clean_avatar %>% gt()
 
-de <- clean_avatar %>% group_by(book_num) %>%
+# Create column for the average IMDB ratings for each season
+with_average <- clean_avatar %>% group_by(book_num) %>%
   mutate(mean_rating = mean(as.numeric(imdb_rating), na.rm = TRUE)) %>% ungroup()
 
-
-drop2 <- c("chapter_num", "director", "imdb_rating")
+# Drop Unwanted Columns and Duplicates
+drop2 <- c("chapter_num", "director", "imdb_rating") # Initially I wanted to have a column for the director most used for each season, but ultimately decided to drop
 gt = de[,!(names(de) %in% drop2)]
 clean_gt <- gt[!duplicated(gt), ]
-clean_gt %>% gt()
 
+# Add column with emojis for each book
 fire <- emo::ji("fire")
 water <- emo::ji("splashing")
 earth <- emo::ji("wood")
+emojis <- c(water, earth, fire) # Put emojis in vector
+clean_gt$new_col <- emojis # Add vector as column to data frame
+gt_table <- clean_gt[,c("book", "new_col", "book_num", "mean_rating")]
 
-emojis <- c(water, earth, fire)
-clean_gt$new_col <- emojis
-small_gt <- clean_gt[,c("book", "new_col", "book_num", "mean_rating")]
-
-small_gt %>% gt() %>%
+# Make GT Table
+gt_table %>% gt() %>%
   tab_header(
     title = "Avatar: The Last Airbender",
     subtitle = "IMDB Ratings by Season") %>%
