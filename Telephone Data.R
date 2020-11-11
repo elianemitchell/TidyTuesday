f6908e4db0383139e9ff7bba7a63be03708309a4
elianemitchell <62623125+elianemitchell@users.noreply.github.com>
2020-11-11 18:05:32 -0500

    Update Telephone Data.R

--- Telephone Data.R
+++ Telephone Data.R
@@ -1,45 +1,36 @@
-# Read in Data
+# read in Data
 tuesdata <- tidytuesdayR::tt_load('2020-11-10')
 mobile <- tuesdata$mobile
 landline <- tuesdata$landline
 
-# Install packages and libraries
+# install packages and libraries
 install.packages("png")
 library(png)
 library(ggplot2)
-install.packages("extrafont")
 library(extrafont)
-font_import()
-y
-fonts()
-fonttable()
-loadfonts()
 
-# Emojis package
-devtools::install_github("hadley/emo")
-
-landline_emoji <- emo::ji("phone")
-mobile_emoji <- emo::ji("cell")
-
-img1 <- readPNG("/Users/eliane/Desktop/telephone.png")
+# save images of mobile phone and landline
+img1 <- readPNG("telephone.png") #image of landline
 image1 <- rasterGrob(img1, interpolate=FALSE)
-
-img2 <- readPNG("/Users/eliane/Desktop/mobile-phone.png")
+img2 <- readPNG("mobile-phone.png") # image of iphone
 image2 <- rasterGrob(img2, interpolate=FALSE)
 
-
-str(mobile)
+# filter US Data 
 us_mobile <- mobile %>% filter(code == "USA")
 us_landline <- landline %>% filter(code == "USA")
-
+# merge US data in each set
 us_data <- inner_join(us_mobile, select(us_landline, year, landline_subs))
 
+# plot line graph
 plot <- ggplot(us_data, aes(x = year)) + 
+  # add images and place them 
   annotation_custom(image2, xmin=2010, xmax=2022, ymin=105, ymax=117) +
   annotation_custom(image1, xmin=2010, xmax=2022, ymin=22, ymax=34) +
+  # draw lines for each kind of phone subscription
   geom_line(aes(y = landline_subs, color = "blue", size = 0.10), show.legend = FALSE) +
   geom_line(aes(y = mobile_subs, color = "red", size = 0.10), show.legend = FALSE) +
   theme_bw() +
+  # add breaks 
   scale_x_continuous(breaks = seq(1990,2017, 4)) +
   labs(title = "Mobile Takeover in the US",
        subtitle = "Phone subscriptions in US per 100 people",
